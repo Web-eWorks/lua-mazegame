8316b9fdef42783b6323002122ff7c05d30a342b
Webster Sheets <Web-eWorks@users.noreply.github.com>
2015-04-07 16:57:53 -0400

    Allowed one letter direction commands, Pt 1

--- script/interpmodules/movemodule.lua
+++ script/interpmodules/movemodule.lua
@@ -4,19 +4,19 @@
 function interpreter.resolveMovement( direction ) 
   local cantGo = function () print("There isn't an exit that way.") end
   
-  if direction == "north" then
+  if direction == "north" or direction == "n" then
     if game.currentRoom.exits.n then
       game.requestTravel(direction)
     else cantGo() end
-  elseif direction == "south" then
+  elseif direction == "south" or direction == "s" then
     if game.currentRoom.exits.s then
       game.requestTravel(direction)
     else cantGo() end
-  elseif direction == "east" then
+  elseif direction == "east" or direction == "e" then
     if game.currentRoom.exits.e then
       game.requestTravel(direction)
     else cantGo() end
-  elseif direction == "west" then
+  elseif direction == "west" or direction == "w" then
     if game.currentRoom.exits.w then
       game.requestTravel(direction)
     else cantGo() end
